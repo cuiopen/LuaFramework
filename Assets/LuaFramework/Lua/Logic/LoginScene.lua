5d16fbf28a1f87b1dc1f4ac569a9622b6b7aef4d
ge <gejiyu@outlook.com>
2017-07-11 17:10:44 +0800

    Clear test

--- Assets/LuaFramework/Lua/Logic/LoginScene.lua
+++ Assets/LuaFramework/Lua/Logic/LoginScene.lua
@@ -1,15 +1,3 @@
-require "3rd/pblua/login_pb"
-require "3rd/pbc/protobuf"
-
-local lpeg = require "lpeg"
-
-local json = require "cjson"
-local util = require "3rd/cjson/util"
-
-local sproto = require "3rd/sproto/sproto"
-local core = require "sproto.core"
-local print_r = require "3rd/sproto/print_r"
-
 require "Logic/LuaClass"
 require "Logic/CtrlManager"
 require "Common/functions"
